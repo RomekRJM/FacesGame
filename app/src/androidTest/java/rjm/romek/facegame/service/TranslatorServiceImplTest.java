2134baa65441226708a2e1db09242e9b266cce97
RomekRJM <rjm_20@o2.pl>
2015-04-22 21:06:59 +0100

    Fix tests.

--- app/src/androidTest/java/rjm/romek/facegame/service/TranslatorServiceImplTest.java
+++ app/src/androidTest/java/rjm/romek/facegame/service/TranslatorServiceImplTest.java
@@ -8,10 +8,10 @@ public class TranslatorServiceImplTest extends TestCase {
     private final TranslatorService translatorService = new TranslatorServiceImpl(Charset.defaultCharset().displayName());
 
     public void testShouldTranslateEncodedDirName() {
-        assertEquals("Côte d’Ivoire", translatorService.translateToName("C%C3%B4te+d%27Ivoire"));
+        assertEquals("Côte d'Ivoire", translatorService.translateToName("C%C3%B4te+d%27Ivoire"));
     }
 
     public void testShouldTranslateDecodedDirName() {
-        assertEquals("C%C3%B4te+d%27Ivoire", translatorService.translateToFileName("Côte d’Ivoire"));
+        assertEquals("C%C3%B4te+d%27Ivoire", translatorService.translateToFileName("Côte d'Ivoire"));
     }
 }
