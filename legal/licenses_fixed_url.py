ecbcd843390b5e6ccaddbccb95e5c55b7cd8e220
RomekRJM <rjm_20@o2.pl>
2015-07-22 23:48:29 +0100

    #59 Prepare licensing document for Google. Url from wiki.

--- legal/licenses_fixed_url.py
+++ legal/licenses_fixed_url.py
@@ -1,20 +1,45 @@
 __author__ = 'roman'
 
-import os;
+import os
 import re
+import urllib
+
+def get_wiki_image(wiki_link):
+
+    if 'http' not in wiki_link:
+        return ''
+
+    content = urllib.urlopen(wiki_link).read()
+    match = None
+
+    try:
+        match = re.search('href="([^"]*)" class="mw-thumbnail-link"', content).group(1)
+    except Exception as e:
+        match = None
+
+    try:
+        if not match:
+            match = re.search('"fullImageLink" id="file"><a href="([^"]*)"', content).group(1)
+    except Exception as e:
+        print wiki_link
+
+    if not match:
+        return ''
+
+    return 'https:' + match
+
 
 try:
     os.remove('image_licenses.html')
 except Exception as e:
     pass
 
-content = None
-
 with open('image_links.html', "r") as f:
     content = f.readlines()
 
 new_content = []
 path = None
+link = None
 
 for line in content:
 
@@ -22,9 +47,17 @@ for line in content:
         match = re.search("'(.*)'", line)
 
         if match:
-            path = match.group(0)
+            path = match.group(1)
 
-        new_line = line
+        new_line = None
+
+    elif '<a href=' in line:
+        match = re.search("'(.*)'", line)
+
+        if match:
+            link = match.group(1)
+
+        new_line = "      <td><img src='" + get_wiki_image(link) + "'/></td>\n" + line
 
     elif '</tr>' in line:
         if path:
@@ -35,7 +68,8 @@ for line in content:
     else:
         new_line = line
 
-    new_content.append(new_line)
+    if new_line:
+        new_content.append(new_line)
 
 
 
